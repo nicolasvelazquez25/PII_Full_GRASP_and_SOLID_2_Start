dcd7ce84f51dba57c9d6c90ee15397e4c6faf370
FabioMario <67033535+FabioMario@users.noreply.github.com>
2020-10-25 17:02:08 -0300

    Agrego clase ConsolePrinter, Interfaz IPrinter y cambio el metodo PrintRecipe()

--- src/Library/Recipe.cs
+++ src/Library/Recipe.cs
@@ -27,10 +27,11 @@ namespace Full_GRASP_And_SOLID.Library
 
         public void PrintRecipe()
         {
-            Console.WriteLine($"Receta de {this.FinalProduct.Description}:");
+            IPrinter printer = new ConsolePrinter();
+            printer.Print($"Receta de {this.FinalProduct.Description}:");
             foreach (Step step in this.steps)
             {
-                Console.WriteLine($"{step.Quantity} de '{step.Input.Description}' " +
+                printer.Print($"{step.Quantity} de '{step.Input.Description}' " +
                     $"usando '{step.Equipment.Description}' durante {step.Time}");
             }
         }
